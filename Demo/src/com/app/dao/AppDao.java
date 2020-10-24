f334c0526e7770f33ec09361579f90d74de0cf78
pc <pc@Aniket>
2020-10-24 20:55:46 +0530

    New changes

--- Demo/src/com/app/dao/AppDao.java
+++ Demo/src/com/app/dao/AppDao.java
@@ -3,7 +3,7 @@ package com.app.dao;
 public class AppDao {
 public void print()
 {
-	System.out.println("Repo1");
+	System.out.println("Repo2");
 
 	System.out.println("Dao1");
     System.out.println("Repo");
