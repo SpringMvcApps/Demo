a3fe07a4aa42403f96e2a0aa8a86d8302898dc92
pc <pc@Aniket>
2020-10-26 21:02:28 +0530

    Service update

--- Demo/src/com/app/service/AppService.java
+++ Demo/src/com/app/service/AppService.java
@@ -7,7 +7,8 @@ public class AppService {
 	public void printMessage()
 	{
 		AppDao dao=new AppDao();
-		dao.print();
+		dao.print();// it calls dao method
+		System.out.println("print service");
 	}
 
 }
